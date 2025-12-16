9d221d4f06fdc9d42f897d328503515e48f482e2
Cory Campbell <campbellcory2360@gmail.com>
2025-12-16 15:41:47 -0500

    feat:Started implemented jwt authentication

---
+++ app/models/token.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+import os
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str

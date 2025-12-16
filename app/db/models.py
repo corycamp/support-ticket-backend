7f3c887ae28fd563e6f8a31ae696587e844fe92a
Cory Campbell <campbellcory2360@gmail.com>
2025-12-16 14:02:43 -0500

    feat:fixed time module used for database created date

--- app/db/models.py
+++ app/db/models.py
@@ -1,7 +1,6 @@
-from app.core.utils import now_iso
 from sqlalchemy import Column, Integer, String, DateTime, Text, Enum as SAEnum
 from sqlalchemy.orm import declarative_base
-from datetime import datetime
+from datetime import datetime, timezone
 from app.models.ticket import Priority, Status
 from app.models.user import Role
 
@@ -14,7 +13,7 @@ class Ticket(Base):
     description = Column(Text)
     priority = Column(SAEnum(Priority), default=Priority.LOW)
     status = Column(SAEnum(Status), default=Status.OPEN)
-    created_at = Column(DateTime, default=now_iso())
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 class Comment(Base):
     __tablename__ = "comments"
@@ -22,11 +21,11 @@ class Comment(Base):
     ticket_id = Column(Integer, nullable=False, index=True)
     user_email = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=now_iso())
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, nullable=False, unique=True, index=True)
     role = Column(SAEnum(Role), nullable=False)
-    created_at = Column(DateTime, default=now_iso())
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
